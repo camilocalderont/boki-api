9153eabbd81a55160fd31587b044152a75971468
Camilo Calderón <cristian.calderontapia@idartes.gov.co>
2025-05-18 17:54:36 -0500

    feat: actualizar esquemas de validación para la creación y actualización de clientes, modificando el patrón del número de teléfono a '57 seguido de 10 dígitos' y actualizando los mensajes de error correspondientes.

--- src/api/modules/client/schemas/clientUpdate.schema.ts
+++ src/api/modules/client/schemas/clientUpdate.schema.ts
@@ -2,9 +2,9 @@ import * as Joi from 'joi';
 import { joiMessagesES } from '../../../shared/utils/joi-messages';
 export const updateClientSchema = Joi.object({
   VcIdentificationNumber: Joi.string().min(5).max(50).required(),
-  VcPhone: Joi.string().pattern(/^(3|6)\d{9}$/).required()
+  VcPhone: Joi.string().pattern(/^57\d{10}$/).required()
   .messages({
-    'string.pattern.base': 'El número de teléfono debe tener 10 dígitos y comenzar con 3 (celular) o 6 (fijo)'
+    'string.pattern.base': 'El número de teléfono debe tener el formato 57 seguido de 10 dígitos (ej: 573102222012)'
   }),
   VcNickName: Joi.string().min(2).max(50),
   VcFirstName: Joi.string().min(2).max(50).required(),
