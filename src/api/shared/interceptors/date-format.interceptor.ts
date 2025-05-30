13f85c791f5c805b47bee5119984a83f084bb5c0
Luisito <supelanosuarezluisenrique5@gmail.com>
2025-05-30 17:59:46 -0500

    refactor: modificar el formato de fecha en DateFormatInterceptor para devolver la fecha en formato 'YYYY-MM-DD HH:mm:ss', incluyendo segundos.

--- src/api/shared/interceptors/date-format.interceptor.ts
+++ src/api/shared/interceptors/date-format.interceptor.ts
@@ -55,12 +55,13 @@ export class DateFormatInterceptor implements NestInterceptor {
   private formatToColombianDate(date: Date): string {
     if (!date) return null;
     
-    const day = date.getDate().toString().padStart(2, '0');
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
     const hours = date.getHours().toString().padStart(2, '0');
     const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
     
-    return `${day}/${month}/${year} ${hours}:${minutes}`;
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
   }
 }
